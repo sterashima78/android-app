2bfc0f7746dc0a28fe83f07a6fbd70e2197ca304
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-11 18:28:22 +0900

    fix JobScheduler imports

--- feature/settings/data/src/main/kotlin/dev/terashima/yomitorirss/feature/settings/data/AiModelDownloadBackground.kt
+++ feature/settings/data/src/main/kotlin/dev/terashima/yomitorirss/feature/settings/data/AiModelDownloadBackground.kt
@@ -1,14 +1,14 @@
 package dev.terashima.yomitorirss.feature.settings.data
 
 import android.annotation.SuppressLint
-import android.app.JobInfo
-import android.app.JobParameters
-import android.app.JobScheduler
-import android.app.JobService
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.job.JobInfo
+import android.app.job.JobParameters
+import android.app.job.JobScheduler
+import android.app.job.JobService
 import android.content.ComponentName
 import android.content.Context
 import android.content.SharedPreferences
