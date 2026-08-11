6b7019d2616803da9d2395f74aec9a444fca6f27
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-11 21:11:04 +0900

    Refresh task widget after task changes

--- app/src/main/java/dev/terashima/yomitorirss/feature/task/TaskRoute.kt
+++ app/src/main/java/dev/terashima/yomitorirss/feature/task/TaskRoute.kt
@@ -1,10 +1,14 @@
 package dev.terashima.yomitorirss.feature.task
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.terashima.yomitorirss.YomitoriApplication
+import dev.terashima.yomitorirss.feature.widget.TaskWidgetUpdater
 
 @Composable
 fun TaskScreen(modifier: Modifier = Modifier) {
@@ -12,5 +16,11 @@ fun TaskScreen(modifier: Modifier = Modifier) {
   val taskViewModel: TaskViewModel = viewModel(
     factory = TaskViewModel.Factory(application.container.taskRepository),
   )
+  val state by taskViewModel.state.collectAsState()
+
+  LaunchedEffect(state.tasks) {
+    TaskWidgetUpdater.updateAll(application)
+  }
+
   TaskScreen(viewModel = taskViewModel, modifier = modifier)
 }
