16515220e9939ebffdd53f190e66f413a8c5ce67
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-14 08:03:20 +0900

    Start Kindle Amazon cover client

---
+++ feature/library/data/src/main/kotlin/dev/terashima/yomitorirss/feature/library/data/KindleAmazonCoverClient.kt
@@ -0,0 +1,8 @@
+package dev.terashima.yomitorirss.feature.library.data
+
+import dev.terashima.yomitorirss.core.network.HttpClient
+import dev.terashima.yomitorirss.core.network.HttpRequest
+import java.io.IOException
+import java.net.URI
+import java.util.Locale
+import org.json.JSONObject
