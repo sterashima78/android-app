48cff246938fd17861f1aa84db1c32ff6e56c3a1
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-14 07:55:14 +0900

    Start Kindle cover enrichment repository

---
+++ feature/library/data/src/main/kotlin/dev/terashima/yomitorirss/feature/library/data/KindleCoverEnrichmentRepository.kt
@@ -0,0 +1,9 @@
+package dev.terashima.yomitorirss.feature.library.data
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import dev.terashima.yomitorirss.core.database.DatabaseConnection
+import dev.terashima.yomitorirss.feature.library.LibraryBook
+import dev.terashima.yomitorirss.feature.library.LibrarySource
+import java.io.IOException
+import org.json.JSONArray
