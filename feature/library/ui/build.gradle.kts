c6b2069905b2ce3c9045414cfa0d0a81c4c19267
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-14 20:58:47 +0900

    Add WebKit dependency for library import

--- feature/library/ui/build.gradle.kts
+++ feature/library/ui/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
   implementation("androidx.compose.foundation:foundation")
   implementation("androidx.compose.material:material-icons-extended")
   implementation("androidx.compose.material3:material3")
+  implementation("androidx.webkit:webkit:1.16.0")
   implementation("io.coil-kt.coil3:coil-compose:3.5.0")
   implementation("io.coil-kt.coil3:coil-network-okhttp:3.5.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
