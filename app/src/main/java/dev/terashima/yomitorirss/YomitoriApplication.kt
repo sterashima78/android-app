1186551c3a70b67139f0a9a8e9de26881f407dc3
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-14 13:06:44 +0900

    refactor: provide composed database schema

--- app/src/main/java/dev/terashima/yomitorirss/YomitoriApplication.kt
+++ app/src/main/java/dev/terashima/yomitorirss/YomitoriApplication.kt
@@ -1,15 +1,20 @@
 package dev.terashima.yomitorirss
 
 import android.app.Application
+import dev.terashima.yomitorirss.core.database.DatabaseSchema
+import dev.terashima.yomitorirss.core.database.DatabaseSchemaProvider
 import dev.terashima.yomitorirss.feature.task.TaskRepository
 import dev.terashima.yomitorirss.feature.widget.TaskRepositoryProvider
 import dev.terashima.yomitorirss.feature.widget.WidgetRepository
 import dev.terashima.yomitorirss.feature.widget.WidgetRepositoryProvider
 
-class YomitoriApplication : Application(), WidgetRepositoryProvider, TaskRepositoryProvider {
+class YomitoriApplication : Application(), WidgetRepositoryProvider, TaskRepositoryProvider, DatabaseSchemaProvider {
   val container: AppContainer by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
     AppContainer(this)
   }
+
+  override val databaseSchema: DatabaseSchema
+    get() = appDatabaseSchema
 
   override val widgetRepository: WidgetRepository
     get() = container.widgetRepository
