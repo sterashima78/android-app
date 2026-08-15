713ac1e9442d46af55c12765eaae1eb256cc47c3
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-15 19:58:24 +0900

    register library database schema

--- app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
+++ app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
@@ -3,17 +3,19 @@ package dev.terashima.yomitorirss
 import dev.terashima.yomitorirss.core.database.DatabaseSchema
 import dev.terashima.yomitorirss.feature.article.data.articleDatabaseSchema
 import dev.terashima.yomitorirss.feature.bookmark.data.bookmarkDatabaseSchema
+import dev.terashima.yomitorirss.feature.library.data.libraryDatabaseSchema
 import dev.terashima.yomitorirss.feature.mail.data.mailDatabaseSchema
 import dev.terashima.yomitorirss.feature.rss.data.rssDatabaseSchema
 import dev.terashima.yomitorirss.feature.summary.data.summaryDatabaseSchema
 
 internal val appDatabaseSchema = DatabaseSchema(
-  version = 13,
+  version = 14,
   contributions = listOf(
     rssDatabaseSchema,
     articleDatabaseSchema,
     bookmarkDatabaseSchema,
     summaryDatabaseSchema,
     mailDatabaseSchema,
+    libraryDatabaseSchema,
   ),
 )
