2aebb18b31df4f5351ade82ec3825d857490c5a3
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-15 22:56:35 +0900

    Bump database for library organization

--- app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
+++ app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
@@ -10,7 +10,7 @@ import dev.terashima.yomitorirss.feature.rss.data.rssDatabaseSchema
 import dev.terashima.yomitorirss.feature.summary.data.summaryDatabaseSchema
 
 internal val appDatabaseSchema = DatabaseSchema(
-  version = 15,
+  version = 16,
   contributions = listOf(
     rssDatabaseSchema,
     articleDatabaseSchema,
