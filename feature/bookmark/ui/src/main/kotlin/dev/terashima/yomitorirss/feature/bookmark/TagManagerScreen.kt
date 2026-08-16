f4c6ee4efdac51d6d23afb17b05d04bda6b6760b
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-17 08:54:22 +0900

    fix: opt in to Material3 bottom sheet API

--- feature/bookmark/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/bookmark/TagManagerScreen.kt
+++ feature/bookmark/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/bookmark/TagManagerScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
 package dev.terashima.yomitorirss.feature.bookmark
 
 import androidx.compose.foundation.layout.Arrangement
