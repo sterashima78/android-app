49925f0efe9bcdccdd4848620e75560f9493ce50
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-16 10:47:32 +0900

    chore: keep current stable Android Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.gradle.api.GradleException
 import org.gradle.api.artifacts.ProjectDependency
 
 plugins {
-  id("com.android.application") version "9.4.0" apply false
-  id("com.android.library") version "9.4.0" apply false
+  id("com.android.application") version "9.3.0" apply false
+  id("com.android.library") version "9.3.0" apply false
   id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
   id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
