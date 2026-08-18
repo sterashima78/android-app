133f66fc047e38cab3d81ab789d4391e33cec1cf
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-19 04:19:56 +0900

    Move knowledge background runtime behind feature boundary

---
+++ app/src/main/java/dev/terashima/yomitorirss/feature/knowledge/KnowledgeBackgroundTypes.kt
@@ -0,0 +1,8 @@
+package dev.terashima.yomitorirss.feature.knowledge
+
+internal typealias KnowledgeBuildTaskSnapshot =
+  dev.terashima.yomitorirss.feature.knowledge.data.KnowledgeBuildTaskSnapshot
+internal typealias KnowledgeBuildTaskState =
+  dev.terashima.yomitorirss.feature.knowledge.data.KnowledgeBuildTaskState
+internal typealias WorkManagerKnowledgeBuildTaskController =
+  dev.terashima.yomitorirss.feature.knowledge.data.WorkManagerKnowledgeBuildTaskController
