4b2dd1c659a32873b945a799ddb0cfc4390b50a8
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-18 22:09:34 +0900

    Fix backup preference Robolectric SDK

--- feature/backup/data/src/test/kotlin/dev/terashima/yomitorirss/backup/BackupPreferencesTest.kt
+++ feature/backup/data/src/test/kotlin/dev/terashima/yomitorirss/backup/BackupPreferencesTest.kt
@@ -9,8 +9,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class BackupPreferencesTest {
   private lateinit var context: Context
 
