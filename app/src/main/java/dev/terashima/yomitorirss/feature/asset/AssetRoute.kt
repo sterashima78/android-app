66a702feeb55c935e739e0be9cca945972d570c9
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-19 06:27:10 +0900

    Inject asset route view model factory

--- app/src/main/java/dev/terashima/yomitorirss/feature/asset/AssetRoute.kt
+++ app/src/main/java/dev/terashima/yomitorirss/feature/asset/AssetRoute.kt
@@ -2,20 +2,14 @@ package dev.terashima.yomitorirss.feature.asset
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import dev.terashima.yomitorirss.YomitoriApplication
 
 @Composable
-fun AssetRoute(modifier: Modifier) {
-  val context = LocalContext.current
-  val application = context.applicationContext as YomitoriApplication
-  val assetViewModel: AssetViewModel = viewModel(
-    factory = AssetViewModel.Factory(
-      repository = application.container.assetRepository,
-      onChanged = application.container.backupChangeScheduler::scheduleAfterChange,
-    ),
-  )
+fun AssetRoute(
+  viewModelFactory: AssetViewModel.Factory,
+  modifier: Modifier,
+) {
+  val assetViewModel: AssetViewModel = viewModel(factory = viewModelFactory)
 
   AssetScreen(
     viewModel = assetViewModel,
