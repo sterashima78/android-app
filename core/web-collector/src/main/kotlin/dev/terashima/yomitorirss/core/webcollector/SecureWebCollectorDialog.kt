4de96a2f80eb4439a58af8d90d0706a294dac42a
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-18 10:24:03 +0900

    Fix Compose weight resolution

--- core/web-collector/src/main/kotlin/dev/terashima/yomitorirss/core/webcollector/SecureWebCollectorDialog.kt
+++ core/web-collector/src/main/kotlin/dev/terashima/yomitorirss/core/webcollector/SecureWebCollectorDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
