a29ba5f7dbab8f8e95246042b9eebea41be8304a
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-18 20:21:45 +0900

    Compose task chat and YouTube schemas

--- app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
+++ app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
@@ -4,14 +4,17 @@ import dev.terashima.yomitorirss.core.database.DatabaseSchema
 import dev.terashima.yomitorirss.feature.article.data.articleDatabaseSchema
 import dev.terashima.yomitorirss.feature.asset.data.assetDatabaseSchema
 import dev.terashima.yomitorirss.feature.bookmark.data.bookmarkDatabaseSchema
+import dev.terashima.yomitorirss.feature.chat.data.chatDatabaseSchema
 import dev.terashima.yomitorirss.feature.knowledge.data.knowledgeDatabaseSchema
 import dev.terashima.yomitorirss.feature.library.data.libraryDatabaseSchema
 import dev.terashima.yomitorirss.feature.mail.data.mailDatabaseSchema
 import dev.terashima.yomitorirss.feature.rss.data.rssDatabaseSchema
 import dev.terashima.yomitorirss.feature.summary.data.summaryDatabaseSchema
+import dev.terashima.yomitorirss.feature.task.data.taskDatabaseSchema
+import dev.terashima.yomitorirss.feature.youtube.data.youtubeDatabaseSchema
 
 internal val appDatabaseSchema = DatabaseSchema(
-  version = 22,
+  version = 23,
   contributions = listOf(
     rssDatabaseSchema,
     articleDatabaseSchema,
@@ -21,5 +24,8 @@ internal val appDatabaseSchema = DatabaseSchema(
     libraryDatabaseSchema,
     knowledgeDatabaseSchema,
     assetDatabaseSchema,
+    taskDatabaseSchema,
+    chatDatabaseSchema,
+    youtubeDatabaseSchema,
   ),
 )
