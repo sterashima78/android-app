119f5a56b6cf30e15407696ea075dc50af94f42d
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-19 21:34:25 +0900

    Summary domainテストへcoroutines依存を追加

--- feature/summary/domain/build.gradle.kts
+++ feature/summary/domain/build.gradle.kts
@@ -11,5 +11,7 @@ dependencies {
   implementation(project(":feature:bookmark:domain"))
   implementation(project(":feature:reddit:domain"))
   implementation(project(":feature:youtube:domain"))
+
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
   testImplementation("junit:junit:4.13.2")
 }
