a74590e552f349bbbdba0e549d328d59cea90af4
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-19 20:57:40 +0900

    Summary domainへenrichment policy依存を追加

--- feature/summary/domain/build.gradle.kts
+++ feature/summary/domain/build.gradle.kts
@@ -9,5 +9,7 @@ kotlin {
 dependencies {
   implementation(project(":feature:article:domain"))
   implementation(project(":feature:bookmark:domain"))
+  implementation(project(":feature:reddit:domain"))
+  implementation(project(":feature:youtube:domain"))
   testImplementation("junit:junit:4.13.2")
 }
