adba258b3ecd471716a79e67a29ec26bd350c361
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-21 12:37:20 +0900

    database: bump schema for SMB metadata normalization

--- app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
+++ app/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
@@ -14,7 +14,7 @@ import dev.terashima.yomitorirss.feature.task.data.taskDatabaseSchema
 import dev.terashima.yomitorirss.feature.youtube.data.youtubeDatabaseSchema
 
 internal val appDatabaseSchema = DatabaseSchema(
-  version = 26,
+  version = 27,
   contributions = listOf(
     rssDatabaseSchema,
     articleDatabaseSchema,
