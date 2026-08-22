1b6abda8798a6e7fa90fa1151ccc663ff5afc04d
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-23 00:13:25 +0900

    Allow Web library title hints

--- feature/library/domain/src/main/kotlin/dev/terashima/yomitorirss/feature/library/WebLibraryMutator.kt
+++ feature/library/domain/src/main/kotlin/dev/terashima/yomitorirss/feature/library/WebLibraryMutator.kt
@@ -1,7 +1,7 @@
 package dev.terashima.yomitorirss.feature.library
 
 interface WebLibraryMutator {
-  suspend fun addWebBook(url: String): LibraryBook
+  suspend fun addWebBook(url: String, titleHint: String? = null): LibraryBook
   suspend fun removeWebBook(book: LibraryBook)
 }
 
