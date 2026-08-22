f42a375ec428a58db72a704d5e2e1052d2d51af8
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-23 08:33:24 +0900

    Create feature ViewModels only for active content

--- app/src/main/java/dev/terashima/yomitorirss/ui/AppFeatureContent.kt
+++ app/src/main/java/dev/terashima/yomitorirss/ui/AppFeatureContent.kt
@@ -44,70 +44,107 @@ internal fun AppFeatureContent(
   onOpenArticle: (Article) -> Unit,
   onOpenWebServer: () -> Unit,
 ) {
-  val rssViewModel: RssViewModel = viewModel(factory = routeDependencies.rssViewModelFactory)
-  val redditViewModel: RedditViewModel = viewModel(factory = routeDependencies.redditViewModelFactory)
-  val feedViewModel: FeedViewModel = viewModel(factory = routeDependencies.feedViewModelFactory)
-  val bookmarkViewModel: BookmarkViewModel = viewModel(factory = routeDependencies.bookmarkViewModelFactory)
-  val mailViewModel: MailViewModel = viewModel(factory = routeDependencies.mailViewModelFactory)
-  val summaryViewModel: SummaryViewModel = viewModel(factory = routeDependencies.summaryViewModelFactory)
-  val backupViewModel: BackupViewModel = viewModel(factory = routeDependencies.backupViewModelFactory)
-  val aiSettingsViewModel: AiSettingsViewModel = viewModel(factory = routeDependencies.aiSettingsViewModelFactory)
-  val chatViewModel: ChatViewModel = viewModel(factory = routeDependencies.chatViewModelFactory)
-  val summarize: (Article) -> Unit = { article -> summaryViewModel.summarize(article) }
-
   when (selectedTab) {
-    MainTab.INTEGRATED -> IntegratedRoute(
-      modifier = modifier,
-      rssViewModel = rssViewModel,
-      redditViewModel = redditViewModel,
-      feedViewModel = feedViewModel,
-      mailViewModel = mailViewModel,
-      youtubeViewModelFactory = routeDependencies.youtubeViewModelFactory,
-      onOpenArticle = onOpenArticle,
-      onSummarize = summarize,
-      onOpenMail = { thread ->
-        mailViewModel.openThread(thread)
-        appViewModel.selectTab(MainTab.MAIL)
-      },
-    )
+    MainTab.INTEGRATED -> {
+      val rssViewModel: RssViewModel = viewModel(factory = routeDependencies.rssViewModelFactory)
+      val redditViewModel: RedditViewModel = viewModel(factory = routeDependencies.redditViewModelFactory)
+      val feedViewModel: FeedViewModel = viewModel(factory = routeDependencies.feedViewModelFactory)
+      val mailViewModel: MailViewModel = viewModel(factory = routeDependencies.mailViewModelFactory)
+      val summaryViewModel: SummaryViewModel = viewModel(factory = routeDependencies.summaryViewModelFactory)
+      IntegratedRoute(
+        modifier = modifier,
+        rssViewModel = rssViewModel,
+        redditViewModel = redditViewModel,
+        feedViewModel = feedViewModel,
+        mailViewModel = mailViewModel,
+        youtubeViewModelFactory = routeDependencies.youtubeViewModelFactory,
+        onOpenArticle = onOpenArticle,
+        onSummarize = summaryViewModel::summarize,
+        onOpenMail = { thread ->
+          mailViewModel.openThread(thread)
+          appViewModel.selectTab(MainTab.MAIL)
+        },
+      )
+    }
 
     MainTab.UNREAD,
-    MainTab.READ_LATER -> RssRoute(
-      modifier = modifier,
-      tab = requireNotNull(selectedTab.rssTab()),
-      rssViewModel = rssViewModel,
-      feedViewModel = feedViewModel,
-      controller = rssController,
-      onOpen = onOpenArticle,
-      onSummarize = summarize,
-      onEditTags = bookmarkEditController::editTags,
-      onMoveFolder = bookmarkEditController::moveFolder,
-    )
+    MainTab.READ_LATER,
+    -> {
+      val rssViewModel: RssViewModel = viewModel(factory = routeDependencies.rssViewModelFactory)
+      val feedViewModel: FeedViewModel = viewModel(factory = routeDependencies.feedViewModelFactory)
+      val summaryViewModel: SummaryViewModel = viewModel(factory = routeDependencies.summaryViewModelFactory)
+      RssRoute(
+        modifier = modifier,
+        tab = requireNotNull(selectedTab.rssTab()),
+        rssViewModel = rssViewModel,
+        feedViewModel = feedViewModel,
+        controller = rssController,
+        onOpen = onOpenArticle,
+        onSummarize = summaryViewModel::summarize,
+        onEditTags = bookmarkEditController::editTags,
+        onMoveFolder = bookmarkEditController::moveFolder,
+      )
+    }
 
     MainTab.REDDIT_UNREAD,
     MainTab.REDDIT_READ_LATER,
-    MainTab.REDDIT_SUBSCRIPTIONS -> RedditRoute(
-      modifier = modifier,
-      tab = requireNotNull(selectedTab.redditTab()),
-      redditViewModel = redditViewModel,
-      controller = redditController,
-      onOpen = onOpenArticle,
-      onSummarize = summarize,
-    )
+    -> {
+      val redditViewModel: RedditViewModel = viewModel(factory = routeDependencies.redditViewModelFactory)
+      val summaryViewModel: SummaryViewModel = viewModel(factory = routeDependencies.summaryViewModelFactory)
+      RedditRoute(
+        modifier = modifier,
+        tab = requireNotNull(selectedTab.redditTab()),
+        redditViewModel = redditViewModel,
+        controller = redditController,
+        onOpen = onOpenArticle,
+        onSummarize = summaryViewModel::summarize,
+      )
+    }
+
+    MainTab.REDDIT_SUBSCRIPTIONS -> {
+      val redditViewModel: RedditViewModel = viewModel(factory = routeDependencies.redditViewModelFactory)
+      RedditRoute(
+        modifier = modifier,
+        tab = requireNotNull(selectedTab.redditTab()),
+        redditViewModel = redditViewModel,
+        controller = redditController,
+        onOpen = onOpenArticle,
+        onSummarize = {},
+      )
+    }
 
     MainTab.SAVED,
-    MainTab.FOLDERS,
     MainTab.TAGS,
-    MainTab.BOOKMARK_IMPORT -> BookmarkRoute(
-      modifier = modifier,
-      tab = requireNotNull(selectedTab.bookmarkTab()),
-      bookmarkViewModel = bookmarkViewModel,
-      editController = bookmarkEditController,
-      onOpen = onOpenArticle,
-      onSummarize = summarize,
-      onMoveToLibrary = routeDependencies.libraryTransfers.moveBookmarkToLibrary,
-      onImportCompleted = { appViewModel.selectTab(MainTab.SAVED) },
-    )
+    -> {
+      val bookmarkViewModel: BookmarkViewModel = viewModel(factory = routeDependencies.bookmarkViewModelFactory)
+      val summaryViewModel: SummaryViewModel = viewModel(factory = routeDependencies.summaryViewModelFactory)
+      BookmarkRoute(
+        modifier = modifier,
+        tab = requireNotNull(selectedTab.bookmarkTab()),
+        bookmarkViewModel = bookmarkViewModel,
+        editController = bookmarkEditController,
+        onOpen = onOpenArticle,
+        onSummarize = summaryViewModel::summarize,
+        onMoveToLibrary = routeDependencies.libraryTransfers.moveBookmarkToLibrary,
+        onImportCompleted = { appViewModel.selectTab(MainTab.SAVED) },
+      )
+    }
+
+    MainTab.FOLDERS,
+    MainTab.BOOKMARK_IMPORT,
+    -> {
+      val bookmarkViewModel: BookmarkViewModel = viewModel(factory = routeDependencies.bookmarkViewModelFactory)
+      BookmarkRoute(
+        modifier = modifier,
+        tab = requireNotNull(selectedTab.bookmarkTab()),
+        bookmarkViewModel = bookmarkViewModel,
+        editController = bookmarkEditController,
+        onOpen = onOpenArticle,
+        onSummarize = {},
+        onMoveToLibrary = routeDependencies.libraryTransfers.moveBookmarkToLibrary,
+        onImportCompleted = { appViewModel.selectTab(MainTab.SAVED) },
+      )
+    }
 
     MainTab.LIBRARY -> LibraryRoute(
       dependencies = routeDependencies.library,
@@ -151,22 +188,32 @@ internal fun AppFeatureContent(
       viewModelFactory = routeDependencies.workoutViewModelFactory,
       modifier = modifier,
     )
-    MainTab.AI_CHAT -> ChatRoute(modifier = modifier, chatViewModel = chatViewModel)
-    MainTab.FEEDS -> FeedRoute(
-      modifier = modifier,
-      feedViewModel = feedViewModel,
-      controller = rssController,
-      onFeedReady = { appViewModel.selectTab(MainTab.FEEDS) },
-    )
-    MainTab.SETTINGS -> SettingsRoute(
-      modifier = modifier,
-      backupViewModel = backupViewModel,
-      aiSettingsViewModel = aiSettingsViewModel,
-      aiTaskQueueRepository = routeDependencies.aiTaskQueueRepository,
-      initialBackgroundFetchWifiOnly = routeDependencies.backgroundFetchWifiOnly(),
-      onBackgroundFetchWifiOnlyChange = routeDependencies::setBackgroundFetchWifiOnly,
-      onOpenWebServer = onOpenWebServer,
-      onNavigate = appViewModel::selectTab,
-    )
+    MainTab.AI_CHAT -> {
+      val chatViewModel: ChatViewModel = viewModel(factory = routeDependencies.chatViewModelFactory)
+      ChatRoute(modifier = modifier, chatViewModel = chatViewModel)
+    }
+    MainTab.FEEDS -> {
+      val feedViewModel: FeedViewModel = viewModel(factory = routeDependencies.feedViewModelFactory)
+      FeedRoute(
+        modifier = modifier,
+        feedViewModel = feedViewModel,
+        controller = rssController,
+        onFeedReady = { appViewModel.selectTab(MainTab.FEEDS) },
+      )
+    }
+    MainTab.SETTINGS -> {
+      val backupViewModel: BackupViewModel = viewModel(factory = routeDependencies.backupViewModelFactory)
+      val aiSettingsViewModel: AiSettingsViewModel = viewModel(factory = routeDependencies.aiSettingsViewModelFactory)
+      SettingsRoute(
+        modifier = modifier,
+        backupViewModel = backupViewModel,
+        aiSettingsViewModel = aiSettingsViewModel,
+        aiTaskQueueRepository = routeDependencies.aiTaskQueueRepository,
+        initialBackgroundFetchWifiOnly = routeDependencies.backgroundFetchWifiOnly(),
+        onBackgroundFetchWifiOnlyChange = routeDependencies::setBackgroundFetchWifiOnly,
+        onOpenWebServer = onOpenWebServer,
+        onNavigate = appViewModel::selectTab,
+      )
+    }
   }
 }
