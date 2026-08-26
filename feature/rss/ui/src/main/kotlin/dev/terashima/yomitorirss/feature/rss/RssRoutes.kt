2b7395ce0dc51a1a0617967e1a80f1a15860e088
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-26 20:40:44 +0900

    fix(rss): import compose state setter

--- feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
+++ feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.terashima.yomitorirss.core.designsystem.PullToRefreshContainer
