261e11de77fb73a82b728178fcfa5dc202596ad9
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-27 06:09:25 +0900

    test: run app lock preferences on supported sdk

--- app/src/test/java/dev/terashima/yomitorirss/AppLockPreferencesTest.kt
+++ app/src/test/java/dev/terashima/yomitorirss/AppLockPreferencesTest.kt
@@ -8,8 +8,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class AppLockPreferencesTest {
   private val context: Context = ApplicationProvider.getApplicationContext()
 
