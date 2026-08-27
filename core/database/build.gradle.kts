dd4227a0c3d6e3d1a37932292af5c3086e9051f7
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-27 15:51:55 +0900

    test: enable database persistence tests

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -20,4 +20,6 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
 
   testImplementation("junit:junit:4.13.2")
+  testImplementation("androidx.test:core-ktx:1.7.0")
+  testImplementation("org.robolectric:robolectric:4.16.1")
 }
