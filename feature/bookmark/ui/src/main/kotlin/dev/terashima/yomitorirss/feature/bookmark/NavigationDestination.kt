3d7db371b56c69028678eec17e53bf3b79379e21
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-27 23:41:30 +0900

    refactor: add bookmark navigation destinations

---
+++ feature/bookmark/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/bookmark/NavigationDestination.kt
@@ -0,0 +1,6 @@
+package dev.terashima.yomitorirss.feature.bookmark
+
+const val BOOKMARKS_ROUTE = "bookmarks"
+const val BOOKMARK_FOLDERS_ROUTE = "bookmarks/folders"
+const val BOOKMARK_TAGS_ROUTE = "bookmarks/tags"
+const val BOOKMARK_IMPORT_ROUTE = "bookmarks/import"
