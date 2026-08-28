5c2528236388896605ec484138416894f04c6f6b
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-28 13:40:28 +0900

    Use task domain provider contract from widget UI

--- feature/widget/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/widget/WidgetRepositoryAccess.kt
+++ feature/widget/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/widget/WidgetRepositoryAccess.kt
@@ -2,10 +2,7 @@ package dev.terashima.yomitorirss.feature.widget
 
 import android.content.Context
 import dev.terashima.yomitorirss.feature.task.TaskRepository
-
-interface TaskRepositoryProvider {
-  val taskRepository: TaskRepository
-}
+import dev.terashima.yomitorirss.feature.task.TaskRepositoryProvider
 
 internal fun Context.requireTaskRepository(): TaskRepository =
   (applicationContext as? TaskRepositoryProvider)?.taskRepository
