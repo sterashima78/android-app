f8a25dc06f0b1423c595bf2294d563fa02fd2cce
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-29 08:53:18 +0900

    fix: expose lint API to detector tests

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -10,6 +10,7 @@ kotlin {
 dependencies {
   compileOnly("com.android.tools.lint:lint-api:32.3.0")
 
+  testImplementation("com.android.tools.lint:lint-api:32.3.0")
   testImplementation("com.android.tools.lint:lint-tests:32.3.0")
   testImplementation(libs.junit4)
 }
