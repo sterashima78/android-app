4a99ffa9b1456f9c24102c75ab6e374e488e5faa
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-28 14:48:17 +0900

    Narrow MainActivity presentation dependencies

--- app/src/main/java/dev/terashima/yomitorirss/MainActivityPresentationDependencies.kt
+++ app/src/main/java/dev/terashima/yomitorirss/MainActivityPresentationDependencies.kt
@@ -1,8 +1,5 @@
 package dev.terashima.yomitorirss
 
-import dev.terashima.yomitorirss.feature.web.LanWebServerController
-
 class MainActivityPresentationDependencies internal constructor(
   val routeDependencies: AppRouteDependencies,
-  val lanWebServerController: LanWebServerController,
 )
