f9c894705c23094d25fa01a25d544ab8d332ed57
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-29 08:48:54 +0900

    fix: use current lint UElementHandler API

--- lint-rules/src/main/kotlin/dev/terashima/yomitorirss/lint/MainActivityFeatureBoundaryDetector.kt
+++ lint-rules/src/main/kotlin/dev/terashima/yomitorirss/lint/MainActivityFeatureBoundaryDetector.kt
@@ -1,5 +1,6 @@
 package dev.terashima.yomitorirss.lint
 
+import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.Category
 import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Implementation
@@ -8,7 +9,6 @@ import com.android.tools.lint.detector.api.JavaContext
 import com.android.tools.lint.detector.api.Scope
 import com.android.tools.lint.detector.api.Severity
 import com.android.tools.lint.detector.api.SourceCodeScanner
-import com.android.tools.lint.detector.api.UElementHandler
 import org.jetbrains.uast.UElement
 import org.jetbrains.uast.UImportStatement
 
