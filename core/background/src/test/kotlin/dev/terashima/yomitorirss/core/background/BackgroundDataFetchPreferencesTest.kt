8bc9c79172be342cacd360850eadfab7ccf9c1be
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-30 19:27:50 +0900

    test: run background Robolectric tests on API 35

--- core/background/src/test/kotlin/dev/terashima/yomitorirss/core/background/BackgroundDataFetchPreferencesTest.kt
+++ core/background/src/test/kotlin/dev/terashima/yomitorirss/core/background/BackgroundDataFetchPreferencesTest.kt
@@ -9,8 +9,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class BackgroundDataFetchPreferencesTest {
   private val context: Context = ApplicationProvider.getApplicationContext()
 
