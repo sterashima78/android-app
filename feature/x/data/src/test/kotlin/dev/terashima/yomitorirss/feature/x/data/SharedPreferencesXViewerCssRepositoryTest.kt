4b75252dd6ba5d39edd5184e1044839fa625f30c
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-30 19:28:04 +0900

    test: run X data Robolectric tests on API 35

--- feature/x/data/src/test/kotlin/dev/terashima/yomitorirss/feature/x/data/SharedPreferencesXViewerCssRepositoryTest.kt
+++ feature/x/data/src/test/kotlin/dev/terashima/yomitorirss/feature/x/data/SharedPreferencesXViewerCssRepositoryTest.kt
@@ -8,8 +8,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class SharedPreferencesXViewerCssRepositoryTest {
   private lateinit var context: Context
 
