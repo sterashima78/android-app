89f871cecf152ea4f632226abfbadc3a49e10a1d
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-07 17:06:17 +0900

    fix(audio): rely on ColumnScope weight extension

--- app/presentation/src/main/kotlin/dev/terashima/yomitorirss/ui/AppRssNavGraph.kt
+++ app/presentation/src/main/kotlin/dev/terashima/yomitorirss/ui/AppRssNavGraph.kt
@@ -2,7 +2,6 @@ package dev.terashima.yomitorirss.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
