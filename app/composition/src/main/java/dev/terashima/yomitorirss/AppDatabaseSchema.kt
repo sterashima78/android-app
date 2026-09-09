95b556c82e44ea2c49ad2e79f5340e1479c3a42b
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-09 13:23:29 +0900

    Advance database schema for custom video providers

--- app/composition/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
+++ app/composition/src/main/java/dev/terashima/yomitorirss/AppDatabaseSchema.kt
@@ -14,7 +14,7 @@ import dev.terashima.yomitorirss.feature.task.data.taskDatabaseSchema
 import dev.terashima.yomitorirss.feature.video.data.videoDatabaseSchema
 
 val appDatabaseSchema = DatabaseSchema(
-  version = 31,
+  version = 32,
   contributions = listOf(
     rssDatabaseSchema,
     articleDatabaseSchema,
