5bb9baa12b5141a8bdb3c2f3776368226da8d26a
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-19 14:11:12 +0900

    chore: update Compose BOM

--- feature/video/ui/build.gradle.kts
+++ feature/video/ui/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
   implementation(project(":feature:library:domain"))
   implementation(project(":core:designsystem"))
 
-  implementation(platform("androidx.compose:compose-bom:2026.06.00"))
+  implementation(platform("androidx.compose:compose-bom:2026.09.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-viewbinding")
   implementation("androidx.compose.foundation:foundation")
