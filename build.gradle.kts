238357f18dcb9017be1953fe849a317c0959a8b6
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-20 15:59:20 +0900

    build: run local tests on JUnit Platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.GradleException
 import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.tasks.testing.Test
 
 plugins {
   id("com.android.application") version "9.4.0" apply false
@@ -603,14 +604,25 @@ val verifyArchitecture by tasks.registering {
 }
 
 subprojects {
+  tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+  }
+
+  fun configureLocalTestPlatform() {
+    dependencies.add("testImplementation", libs.junit4)
+    dependencies.add("testImplementation", libs.junit.jupiter)
+    dependencies.add("testRuntimeOnly", libs.junit.vintage.engine)
+    dependencies.add("testRuntimeOnly", libs.junit.platform.launcher)
+  }
+
   pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-    dependencies.add("testImplementation", "junit:junit:4.13.2")
+    configureLocalTestPlatform()
   }
   pluginManager.withPlugin("com.android.library") {
-    dependencies.add("testImplementation", "junit:junit:4.13.2")
+    configureLocalTestPlatform()
   }
   pluginManager.withPlugin("com.android.application") {
-    dependencies.add("testImplementation", "junit:junit:4.13.2")
+    configureLocalTestPlatform()
 
     extensions.configure<ApplicationExtension> {
       defaultConfig {
