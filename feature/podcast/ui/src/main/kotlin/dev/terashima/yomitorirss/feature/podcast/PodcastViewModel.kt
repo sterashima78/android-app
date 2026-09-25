ac0b7ace4c506791237a4f39cef3247182765a9b
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-25 13:11:12 +0900

    fix: import podcast change collector

--- feature/podcast/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/podcast/PodcastViewModel.kt
+++ feature/podcast/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/podcast/PodcastViewModel.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
