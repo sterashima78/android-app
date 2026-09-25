41003b5789db1440872e8f3bd13ebbc8557a1dc8
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-25 17:45:52 +0900

    RSS設定タブへ推薦フィルタ設定を追加

--- feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
+++ feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
@@ -1,6 +1,7 @@
 package dev.terashima.yomitorirss.feature.rss
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
