a46a186f79e4d5ebd8056b54da0b7cc1cc8294a6
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-25 17:47:20 +0900

    RSS routeの未使用importを整理

--- feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
+++ feature/rss/ui/src/main/kotlin/dev/terashima/yomitorirss/feature/rss/RssRoutes.kt
@@ -1,7 +1,6 @@
 package dev.terashima.yomitorirss.feature.rss
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
